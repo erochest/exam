0424a70ecea6904c12e8955dd1aedee1955acdac
Eric Rochester <eric.rochester@willowtreeapps.com>
2019-09-13 05:38:23 -0400

    Added security.

---
+++ src/main/java/com/willowtree/exam/MvcConfiguration.java
@@ -0,0 +1,15 @@
+package com.willowtree.exam;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello-page").setViewName("hello-page");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
